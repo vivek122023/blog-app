8a8f3cdb0fef31da3e5049a7e46388c2a224d898
Vivek Kumar <vivekkumar10560@gmail.com>
2026-01-08 17:29:39 +0530

    fix: replace localhost with live backend url

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:5173';
-// export const BACKEND_URL = 'https://blog-app-a2ja.onrender.com';
+// export const BACKEND_URL = 'http://localhost:5173';
+export const BACKEND_URL = 'https://blog-app-a2ja.onrender.com';
