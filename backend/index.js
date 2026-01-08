1098791d9ec30f66680b6dba89dd797d9e511a27
Vivek Kumar <vivekkumar10560@gmail.com>
2026-01-08 17:27:44 +0530

    fix: replace localhost with live frontend url

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(
   cors({
-    origin: process.env.Frontend_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
